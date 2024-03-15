436a198d443ba4f350732d003571ddff1351763e
kelleighm <kelleighmmartin@gmail.com>
2024-03-14 20:45:15 -0500

    Exercise

--- part2/studio/src/components/RateARecipe.jsx
+++ part2/studio/src/components/RateARecipe.jsx
@@ -1,7 +1,14 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
-  return stars;
+function RateARecipe(props) {
+
+    function GiveRating() {
+      return <h3>{stars[props.rating - 1]}</h3>;
+      }
+
+    const validNum = props.rating > 0 && props.rating < 6; 
+    return validNum ? <GiveRating /> : null; 
+
 }
 
 export default RateARecipe;
